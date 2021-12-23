e713ab9b1c878942a1ffd3a654d953ae7af167aa
Giwoo G Lee <gglee89@gmail.com>
2021-12-23 10:09:03 +0900

    refactor: Integrate typescript

---
+++ build/routes/login-routes.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+exports.router = router;
+router.post('/api/login', (req, res) => {
+    const { email, password } = req.body;
+    if (!email) {
+        return res.status(400).send({ message: "'email' is invalid", status: 400 });
+    }
+    if (!password) {
+        return res.status(400).send({ message: "'password' is invalid", status: 400 });
+    }
+    req.session = { loggedIn: true };
+    return res.status(200).send({ message: 'logged-in' });
+});
---
+++ src/routes/login-routes.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from 'express'
+
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined }
+}
+
+const router = Router()
+
+router.post('/api/login', (req: RequestWithBody, res: Response) => {
+    const { email, password } = req.body
+
+    if (!email) {
+        return res.status(400).send({ message: "'email' is invalid", status: 400 })
+    }
+
+    if (!password) {
+        return res.status(400).send({ message: "'password' is invalid", status: 400 })
+    }
+
+    req.session = { loggedIn: true }    
+
+    return res.status(200).send({ message: 'logged-in' });
+})
+
+export { router }
