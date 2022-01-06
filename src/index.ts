04d765518e6e8c200f24dc5b8d8432b283a81db0
Giwoo G Lee <hazel@Giwoo-Lee.local>
2022-01-07 02:11:51 +0900

    feat: Handle CORS

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import express, { Request, Response } from 'express'
-import { router } from './routes/login-routes'
-import bodyParser from 'body-parser'
-import cookieSession from 'cookie-session'
-import dotenv from 'dotenv'
+import express from 'express';
+import { router } from './routes/app-routes';
+import cookieSession from 'cookie-session';
+import dotenv from 'dotenv';
+import cors, { CorsRequest } from 'cors';
 
-const app = express()
+const app = express();
 
-dotenv.config()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieSession({ keys: ['lasdfk'] }))
-app.use(router)
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieSession({ keys: ['lasdfk'] }));
+app.use(
+  cors((req: CorsRequest, callback) => {
+    let corsOptions = { origin: false };
+    if (['http://localhost:3000'].indexOf(req.headers.origin || '') !== -1) corsOptions = { origin: true };
+    return callback(null, corsOptions);
+  }),
+);
+app.use(router);
 
 app.listen(5001, () => {
-    console.log('listening on port 5001')
-})
+  console.log('listening on port 5001');
+});
